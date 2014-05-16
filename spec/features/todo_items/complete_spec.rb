c99d5c20404d5c7bc5dea8c6b80a80287545115d
Krs Joseph <krspjoseph@gmail.com>
2014-05-16 06:32:19 -0400

    Items can now be marked as complete

---
+++ spec/features/todo_items/complete_spec.rb
@@ -0,0 +1,19 @@
+require 'spec_helper'
+
+describe "Completing todo items" do
+  let!(:todo_list) {TodoList.create(title: "My List", description: "My list item")}
+  let!(:todo_item) {todo_list.todo_items.create(content: "Milk")}
+
+  it "is successful when marking a single item complete" do
+    expect(todo_item.completed_at).to be_nil
+    visit_todo_list todo_list
+    within dom_id_for(todo_item) do
+      click_link "Mark Completed"
+    end
+    todo_item.reload
+    expect(todo_item.completed_at).to_not be_nil
+
+  end
+
+
+end
